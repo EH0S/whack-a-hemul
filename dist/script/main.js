681f087320c76c61288d8ea750783124a9048139
GrowTaz <toipuh@gmail.com>
2023-09-14 16:42:39 +0300

    level system with different speeds + fixed bg & cursor + lifes system + fixed multiple games running at same time when pressing start button

--- dist/script/main.js
+++ dist/script/main.js
@@ -8,11 +8,24 @@ let pojoja = 0
 let isDead = false;
 var audio = new Audio('./sound/gnome.mp3');
 var death = new Audio('./sound/death.mp3');
-
+let playerPosition = { x: -1, y: -1 }; 
+let enemyPosition = { x: -1, y: -1 }; 
 
 let bestScore = localStorage.getItem('bestScore') || 0; 
 const scoreElement = document.getElementById('score');
 const bestScoreElement = document.getElementById('best-score');
+const lifes = document.getElementById('life');
+let lifeAmount = 3;
+let hittedHemuls = 0;
+let missedHemuls = 0;
+
+const audioElement = document.getElementById('myAudio');
+
+function changePlaybackSpeed(speed) {
+    if (audioElement) {
+        audioElement.playbackRate = speed;
+    }
+}
 
 
 function chooseMusic(e){ // PLAY RANDOM SONG WHEN GAME START AND KEEP PLAYING THE LIST
@@ -102,6 +115,7 @@ function start() {
     inGame = true;
     fillCanvases();  
     gameLoop();
+    audioElement.play();
     //chooseMusic();
     
     //drawPlayer();
@@ -116,7 +130,7 @@ function drawPlayer() {
 
     const canvas = grid[x][y];
     const ctx = canvas.getContext('2d');
-
+    playerPosition = { x, y };
     ctx.fillStyle = "#C5DCFF";
     canvas.id = 'hemul';
     //ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -152,7 +166,7 @@ function drawEnemy(){
     const y = getRandomInt(4);
     const canvas = grid[x][y];
     const ctx = canvas.getContext('2d');
-
+    enemyPosition = { x, y };
     canvas.id = 'enemy';
     ctx.fillStyle = "#C5DCFF";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -194,19 +208,24 @@ function CanvasClicked(event) {
     console.log('score increased by one\nTotal:',pojoja);
     
     audio.play();
-    
+    hittedHemuls += 1;
     animateCanvasRemoval(canvas);
     
   }
   else if (inGame && canvas.id != "hemul") {
     console.log('l bozo'); // if empty square clicked do this...
+        lifeAmount = lifeAmount - 1;
+        lifes.textContent = lifeAmount;
+        if (lifeAmount < 1) {
+            death.play();
+
+    
+            isDead = true;
+            alert('game over');
+            window.location.reload();
+    }
   }
   if (canvas.id === "enemy"){
-    death.play();
-    death.play();
-    death.play();
-    death.play();
-    death.play();
     death.play();
 
     
@@ -247,6 +266,7 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
  const time = 10
  while (time > 0 && isDead === false && inGame) {
     drawPlayer();
+    
     const enemySpawnerChance = getRandomInt(5);
     const enemySpawnerAmount = getRandomInt(4);
     if (enemySpawnerChance === 2){
@@ -276,14 +296,118 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
         
     }
    // checkForClicks();
-    await delay(Math.floor((Math.random() * 1000) + 800)); 
+   const checkScore = levels();
+   const blinkingDelay = 500
+
+   //checkScore = levels();
+    await delay(checkScore); 
+    missedHemuls += 1;
+
+    if (hittedHemuls != missedHemuls){
+        death.play();
+
+    
+        isDead = true;
+        alert('game over');
+        window.location.reload();
+    }
+
     fillCanvases();
-    await delay(500); //blinking delay - can be removed if annoying
+    //await delay(blinkingDelay); //blinking delay - can be removed if annoying
 
     //infinite game loop so continue here with countdown timer or score system...
  }
  // end game
 
+}
+
+
+const hemul = {
+    speedDefault: Math.floor((Math.random() * 2500) + 1000),
+    speedLvl1: Math.floor((Math.random() * 2000) + 1000),
+    speedLvl2: Math.floor((Math.random() * 1500) + 1000),
+    speedLvl3: Math.floor((Math.random() * 1000) + 800),
+    speedLvl4: Math.floor((Math.random() * 1000) + 500),
+    speedLvl5: Math.floor((Math.random() * 900) + 500),
+    speedLvl6: Math.floor((Math.random() * 800) + 500),
+    speedLvl7: Math.floor((Math.random() * 600) + 350),
+};
+function changePlaybackSpeed(speed) {
+    audioElement.playbackRate = speed;
+  }
+function levels(){
+
+
+    if (pojoja >= 5 && pojoja < 10){
+        console.log("Speed: Lvl1");
+        checkScore = hemul.speedLvl1;
+        changePlaybackSpeed(1.2)
+        for (const cloud of clouds) {
+            cloud.speed = 0.1;
+        }
+        
+        return checkScore;
+    }
+    if (pojoja >= 10 && pojoja < 15){
+        console.log("Speed: Lvl2");
+        checkScore = hemul.speedLvl2;
+        changePlaybackSpeed(1.5)
+        for (const cloud of clouds) {
+            cloud.speed = 0.2;
+        }
+        return checkScore;
+    }
+    if (pojoja >= 15 & pojoja < 20){
+        console.log("Speed: Lvl3");
+        checkScore = hemul.speedLvl3;
+        changePlaybackSpeed(2)
+        for (const cloud of clouds) {
+            cloud.speed = 0.3;
+        }
+        return checkScore;
+    }
+    if (pojoja >= 20 & pojoja < 25){
+        console.log("Speed: Lvl4");
+        checkScore = hemul.speedLvl4;
+        changePlaybackSpeed(2.1)
+        for (const cloud of clouds) {
+            cloud.speed = 0.4;
+        }
+        return checkScore;
+    }
+    if (pojoja >= 25 & pojoja < 30){
+        console.log("Speed: Lvl5");
+        checkScore = hemul.speedLvl5;
+        changePlaybackSpeed(2.3)
+        for (const cloud of clouds) {
+            cloud.speed = 1;
+        }
+        return checkScore;
+    }
+    if (pojoja >= 30 & pojoja < 35){
+        console.log("Speed: Lvl6");
+        checkScore = hemul.speedLvl6;
+        changePlaybackSpeed(2.5)
+        for (const cloud of clouds) {
+            cloud.speed = 1.5;
+        }
+        return checkScore;
+    }
+    if (pojoja >= 35){
+        console.log("Speed: Lvl7");
+        checkScore = hemul.speedLvl7;
+        changePlaybackSpeed(2.7)
+        for (const cloud of clouds) {
+            cloud.speed = 2;
+        }
+        return checkScore;
+    }
+    if (pojoja < 5) {
+        console.log("Speed: Default");
+        checkScore = hemul.speedDefault;
+        return checkScore;
+    }
+    
 }
 
 function clear() {
@@ -312,18 +436,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const newGameButton = document.getElementById("newGame");
 
     startButton.onclick = function() {
-        pojoja = 0;
-        updateScore();
-        start();
+
+        if (inGame == false) {
+            pojoja = 0;
+            updateScore();
+            start();   
+        }
+        
         
     };
 
     newGameButton.onclick = function() {
         
-        pojoja = 0;
-        updateScore();
-        inGame = false;
-        clear();
+        if (inGame){
+            pojoja = 0;
+            updateScore();
+            inGame = false;
+            clear();
+        }
+
     };
 });
 
@@ -368,17 +499,23 @@ const c = cloudCanvas.getContext('2d');
 
 // Define an array to hold cloud objects
 const clouds = [
-  { image: document.getElementById('cloud1'), x: 0, y: 50, speed: 0.01 },
+  { image: document.getElementById('cloud1'), x: 0, y: 50, speed: 0.02 },
+ 
   { image: document.getElementById('cloud2'), x: 40, y: 30, speed: 0.01 },
-  { image: document.getElementById('cloud1'), x: 50, y: 60, speed: 0.01 },
-  { image: document.getElementById('cloud2'), x: 85, y: 40, speed: 0.01 },
+  { image: document.getElementById('cloud1'), x: 50, y: 60, speed: 0.02 },
+
+  { image: document.getElementById('cloud2'), x: 85, y: 40, speed: 0.02 },
   { image: document.getElementById('cloud1'), x: 0, y: 90, speed: 0.01 },
+  
 
   { image: document.getElementById('cloud1'), x: 200, y: 50, speed: 0.01 },
-  { image: document.getElementById('cloud2'), x: 240, y: 30, speed: 0.01 },
+  
+  { image: document.getElementById('cloud2'), x: 240, y: 30, speed: 0.02 },
   { image: document.getElementById('cloud1'), x: 250, y: 60, speed: 0.01 },
+
   { image: document.getElementById('cloud2'), x: 285, y: 40, speed: 0.01 },
-  { image: document.getElementById('cloud1'), x: 200, y: 90, speed: 0.01 },
+  { image: document.getElementById('cloud1'), x: 200, y: 90, speed: 0.02 },
+
 
 
   //(scalable!!)
